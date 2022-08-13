51d68c1873bdf606074db136fb79a6974b0c9877
epsilon-deltta <kokomong1316@gmail.com>
2022-08-13 21:31:55 +0900

    Fixed: rel path error

--- ppg_pre/utils.py
+++ ppg_pre/utils.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import os
+
+cur_dir = os.path.dirname(os.path.realpath(__file__))
+
 def get_ppg_sample(idx=0):
-    path = f'./sample/ppg{idx}.csv'
+
+    path = os.path.join(cur_dir, f'./sample/ppg{idx}.csv')
     df   = pd.read_csv(path,index_col=0)
     return df.to_numpy()
